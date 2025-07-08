870d3e1da70fed151cef944f961fcaa593479d5a
James Patrick De Mesa <83571819+SaucesCode@users.noreply.github.com>
2025-07-08 18:02:12 +0800

    Update settings.py

--- imageGallery/settings.py
+++ imageGallery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$rq1(c0$3xv6ks_a-3abjx6x2*@m*b9ejd0+)m!8!6#zl0i31l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
